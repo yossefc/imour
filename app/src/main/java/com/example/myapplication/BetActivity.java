e4bebd398ddd4c4c7f3f9871cd477d5d968e3729
yossefc <45142394+yossefc@users.noreply.github.com>
2026-01-11 16:20:24 +0200

    Replace TextView import with EditText import

--- app/src/main/java/com/example/myapplication/BetActivity.java
+++ app/src/main/java/com/example/myapplication/BetActivity.java
@@ -2,7 +2,7 @@ package com.example.myapplication;
 
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
