8c9f7e05b6fb7e1b347805cacc2d6e72d18a8d2a
yossefc <45142394+yossefc@users.noreply.github.com>
2026-01-11 16:27:38 +0200

    Update BetActivity.java

--- app/src/main/java/com/example/myapplication/BetActivity.java
+++ app/src/main/java/com/example/myapplication/BetActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
